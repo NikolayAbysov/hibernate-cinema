ef4ca16889c550d3296e29286233db2b6d258fbb
NikolayAbysov <nikolay.abysov@ukr.net>
2020-05-25 15:14:23 +0300

    Issues fixed

--- src/main/java/com/dev/cinema/util/HashUtil.java
+++ src/main/java/com/dev/cinema/util/HashUtil.java
@@ -6,6 +6,7 @@ import java.security.SecureRandom;
 import org.apache.log4j.Logger;
 
 public class HashUtil {
+    private static final String ALGORITHM = "SHA-512";
     private static Logger logger = Logger.getLogger(HashUtil.class);
 
     public static byte[] getSalt() {
@@ -18,7 +19,7 @@ public class HashUtil {
     public static String hashPassword(String password, byte[] salt) {
         StringBuilder hashedPassword = new StringBuilder();
         try {
-            MessageDigest messageDigest = MessageDigest.getInstance("SHA-512");
+            MessageDigest messageDigest = MessageDigest.getInstance(ALGORITHM);
             messageDigest.update(salt);
             byte[] digest = messageDigest.digest(password.getBytes());
             for (byte b : digest) {
